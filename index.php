05141b929a4171a22d3de94d35e6d44937000fda
CodeLIebe <efughe@gmail.com>
2019-03-25 22:10:14 +0100

    commit

--- index.php
+++ index.php
@@ -18,11 +18,22 @@ get_header(); ?>
     <div class="col-7 p-5">
       <?php
       if ( have_posts() ) : while ( have_posts() ) : the_post();
-          ?> <h2> <?php the_title(); ?> </h2>
+          ?>
+          <h2> <?php the_title(); ?> </h2>
           <hr  />
           <i><small>By <?php the_author_posts_link(); ?> on <?php the_time('F jS, Y'); ?>  in <?php the_category(', '); ?> <?php edit_post_link(__('{Edit}'), ''); ?></small></i>
-          <?php the_content();
-      endwhile;
+          <?php the_content(); ?>
+          <?php if( $tags = get_the_tags() ) {
+            foreach( $tags as $tag ) {
+                $sep = ( $tag === end( $tags ) ) ? '' : ', ';
+                echo '<a href="' . get_term_link( $tag, $tag->taxonomy ) . '">#' . $tag->name . ' </a>';
+            }
+          }?>
+        <br />
+        <a href="<?php echo esc_url( get_permalink( get_previous_post()->ID ) ); ?>"><< prev </a>
+        |
+        <a href="<?php echo esc_url( get_permalink( get_next_post()->ID ) ); ?>">next >> </a>
+      <?php endwhile;
       else :
           _e( 'Sorry, nothing to see here. Mind your own business, bro!', 'textdomain' );
       endif;
